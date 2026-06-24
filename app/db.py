791077de046bf7a204ae72157caeaa9fa4205fba
Kartik Batchu <kartikvaids121@siesgst.ac.in>
2026-06-24 08:07:30 +0530

    Assessment Handling

--- app/db.py
+++ app/db.py
@@ -12,9 +12,14 @@ async def init_db():
     _pool = await asyncpg.create_pool(
         dsn=os.getenv("DATABASE_URL"),
         min_size=2,
-        max_size=10
+        max_size=10,
+        init=_set_timezone
     )
     print("DB connected")
+
+
+async def _set_timezone(conn):
+    await conn.execute("SET timezone = 'Asia/Kolkata'")
 
 
 async def close_db():
